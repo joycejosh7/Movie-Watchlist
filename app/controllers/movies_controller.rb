794aff03acf4ad4a581824753156f51d41cbfd72
Joshua <joycejosh7@gmail.com>
2021-01-04 15:27:07 -0500

    added movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,3 +1,25 @@
-class MovieController < ApplicationController
-    
+class MoviesController < ApplicationController
+    get '/movies' do
+        @movies = Movie.all 
+        erb :'movies/index'
+    end
+
+    get '/movies/new' do
+        erb :'movies/new'
+    end
+
+    get '/movies/:id' do
+        @movie = Movie.find_by_id(params[:id])
+        erb :'movies/show'
+    end
+
+    post '/movies' do
+        movie = Movie.new(params[:movie])
+
+        if movie.save
+            redirect '/movies'
+        else  
+            redirect '/movies/new'
+        end
+    end
 end
