8dcabcdf22bee61708aa0b76260c1abce2552a2b
Joshua <joycejosh7@gmail.com>
2021-01-05 13:52:06 -0500

    added users

--- config.ru
+++ config.ru
@@ -6,5 +6,6 @@ end
 
 use Rack::MethodOverride
 
+use SessionsController
 use MoviesController
 run ApplicationController
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210104163235) do
+ActiveRecord::Schema.define(version: 20210105185137) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20210104163235) do
     t.string   "genre"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "useranme"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
