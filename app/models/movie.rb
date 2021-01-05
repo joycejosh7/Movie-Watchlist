a051e19648a561aecc2b180549c1f95f56790336
Joshua <joycejosh7@gmail.com>
2021-01-05 15:03:46 -0500

    added relationship between movie and user models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,5 @@
 class Movie < ActiveRecord::Base
+    belongs_to :user
+
     validates_presence_of :title, :release_date, :genre
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+    has_many :movies
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: true
 end
