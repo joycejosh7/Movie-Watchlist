4868b326a49dc5447cdba810c2d7343dd17e385e
Joshua <joycejosh7@gmail.com>
2021-01-05 15:02:57 -0500

    corrected typo and remigrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210105185137) do
+ActiveRecord::Schema.define(version: 20210105185321) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,10 +19,11 @@ ActiveRecord::Schema.define(version: 20210105185137) do
     t.string   "genre"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "useranme"
+    t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
